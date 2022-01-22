40a27b80e3dacc98b658596308543d73997313e2
NIKLAS FRIDMAN <fridman.niklas@gmail.com>
2022-01-22 14:40:01 +0100

    added html css for index and create action

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,37 @@
+class FlatsController < ApplicationController
+  before_action :find_flat, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @flats = Flat.all
+  end
+
+  def show
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def destroy
+    @flat.destroy
+    redirect_to flat_path, notice: 'Restaurant was successfully destroyed.'
+  end
+
+  def create
+    @flat = Flat.create(flat_params)
+    if @flat.save
+      redirect_to flat_path(@flat)
+    else
+      render :new
+  end
+end
+
+  private
+  def find_flat
+    @flat = Flat.find(params[:id])
+  end
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests )
+  end
+end
